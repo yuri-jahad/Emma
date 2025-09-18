ebf97fde5f1cc7f59c291c6a26987685a2e02f9d
MK <119771985+yuri-jahad@users.noreply.github.com>
2025-09-19 01:45:03 +0200

    feat(init) Initialize discord server
    generate user profile - token

--- index.ts
+++ index.ts
@@ -1,2 +1,11 @@
-console.log("Hello via Bun!");
-console.log('bonjour les amis')
+import { Client, Events, GatewayIntentBits } from 'discord.js'
+
+const token = process.env.DISCORD_TOKEN
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] })
+
+client.once(Events.ClientReady, readyClient => {
+  console.log(`Ready! logged in as ${readyClient.user.tag}`)
+})
+
+client.login(token)
